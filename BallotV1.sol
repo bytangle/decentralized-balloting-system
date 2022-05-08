f0876090a6b7dae58507f0421674c34ad788c865
Bytangle <dev.bytangle@gmail.com>
2022-05-08 23:56:06 +0100

    Implement functions

--- BallotV1.sol
+++ BallotV1.sol
@@ -17,5 +17,28 @@ contract Ballot {
     Proposal[] proposals;
 
     enum Phase {Init, Regs, Vote, Done};
-    Phase public state = Phase.Init;
+    Phase private state = Phase.Init;
+
+    /** Gate Keeper */
+    modifier onlyChairperson {
+        require(chairperson == msg.sender);
+        _;
+    }
+
+    constructor(uint _proposals) public {
+        chairperson = msg.sender; // Contract deployer is the admin
+        voters[chairperson].weight = 2; // Chairperson has the highest vote weight
+
+        /** Initial Proposals */
+        for(uint proposal = 0; proposal < _proposals; proposal++) {
+            proposals.push(Proposal(0)); // Create proposal and save it in the proposals array
+        }
+    }
+
+    function changePhase(Phase phase) public onlyChairperson {
+        if(msg.sender != chairperson) { revert(); } /** Revert if entity calling this function isn't the chairperson */
+        if(x < state) { revert(); } /** State transition allowed : 0 -> 1 -> 2 -> 3 */
+
+        state = phase;
+    }
 }
